f25458e43e5b120cb785a6711f3beecf6bef4455
wds <wudongshuo@hotmail.com>
2017-07-11 19:15:02 +0800

    add express

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+<<<<<<< HEAD
 router.get('/', function(req, res, next) {
+=======
+router.get('/', function(req, res) {
+>>>>>>> add express
   res.render('index', { title: 'Express' });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+<<<<<<< HEAD
 router.get('/', function(req, res, next) {
+=======
+router.get('/', function(req, res) {
+>>>>>>> add express
   res.send('respond with a resource');
 });
 
