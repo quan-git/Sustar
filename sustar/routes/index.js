1f9d7d5de05b7e2190c60e340efffaf4b0b469ef
tietoushaonian <bug100000@outlook.com>
2017-07-13 23:09:02 +0800

    修改：     app.js 	修改：     bin/www 	新文件：   db/questions.js 	新文件：   db/users.js 	修改：     package.json 	新文件：   public/css/header.css 	新文件：   public/css/index.css 	新文件：   public/css/info.css 	新文件：   public/css/log in.css 	新文件：   public/img/dong.gif 	新文件：   public/js/style.js 	新文件：   public/script/search.js 	修改：     public/stylesheets/search.css 	修改：     public/stylesheets/style.css 	修改：     routes/index.js 	新文件：   routes/quit.js 	修改：     routes/search.js 	新文件：   routes/zhong.js 	新文件：   views/foot.ejs 	新文件：   views/header.ejs 	修改：     views/index.ejs 	新文件：   views/login.ejs 	修改：     views/search.ejs

--- sustar/routes/index.js
+++ sustar/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+  console.log(req.session.uid);
+  if (req.session.uid != undefined) {
+    res.render('index', {state: "true"});
+  }else {
+    res.render('index', {state: "false"});
+  }
 });
 
 module.exports = router;
--- sustar/routes/search.js
+++ sustar/routes/search.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../db/questions.js');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('search', { title: 'Search' });
+  var result = [];
+  var j = 0;
+  if (req.query.searchtext != undefined) {
+    var key = new RegExp(req.query.searchtext, "gi");
+    //提取拿出数据库问题集合中的所有文档
+    db.qdb.find({},function(err, data){
+      for (var i = 0; i < data.length; i++) {
+        if (key.test(data[i])) {
+          //将数据与用户匹配的文档全部放入result中
+          result[j] = data[i];
+          j++;
+        };
+      };
+      if (result[0] == null) {
+        result = [{"title":"没有匹配到问题！"}];
+      }
+      res.render('search', { result: result });
+    });
+  }else {
+    res.render('search', { result: result });
+  };
 });
 
 module.exports = router;
